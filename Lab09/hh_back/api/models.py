9527b96ac4acfa147b07f964e91b2bf7d6cb97b3
Nuridin <nurmannuridin28@gmail.com>
2025-04-09 09:42:20 +0500

    lab10

--- Lab09/hh_back/api/models.py
+++ Lab09/hh_back/api/models.py
@@ -20,11 +20,28 @@ class Company(models.Model):
             "address": self.address
         }
 
+
+    
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+    
 class Vacancy(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     salary = models.FloatField()
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='vacancies')
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name='position')
 
     def __str__(self):
         return self.name
--- Lab09/hh_back/api/urls.py
+++ Lab09/hh_back/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from api.views import companies_list, company_detail, company_vacancies, top_ten_vacancies, vacancies_list, vacancy_detail
+from api.views import companies_list, company_detail, company_vacancies, post_vacancy, show_all_positions, show_position_by_id, top_ten_vacancies, vacancies_list, vacancy_detail
 
 urlpatterns = [
     path('companies/', companies_list),
@@ -9,4 +9,7 @@ urlpatterns = [
     path('vacancies/', vacancies_list),
     path('vacancies/<int:id>/', vacancy_detail),
     path('vacancies/top_ten/', top_ten_vacancies),
+    path('positions/', show_all_positions),
+    path('positions/<int:id>/', show_position_by_id),
+    path('vacancy/', post_vacancy)
 ]
